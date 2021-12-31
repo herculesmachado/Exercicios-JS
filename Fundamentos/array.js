f676468a7b1e807988d5054462605d75801778e4
Hércules Machado <herculesprogramador911@gmail.com>
2021-12-31 18:20:04 -0300

    Atualização e correção de erros

--- Fundamentos/array.js
+++ Fundamentos/array.js
@@ -6,12 +6,14 @@ console.log(valores)
 
 console.log(valores.length) //Mostra a quantidade de elementos a dentro de uma array
 
-valores.push({id: 3}, null, false, 'Texto') //Adiciona coisas na array
+valores.push({id /*Identificador*/ : 3}, null, false, 'Texto') //Adiciona coisas na array
 console.log(valores)
 
 console.log(valores.pop()) //retira o ultimo objeto da array
+
 delete valores[0] //Delete um objeto da array
 console.log(valores)
+
 console.log(typeof valores) //Mostra o tipo da array
 
-//OBS: para a saude do código e de quem esta lendo, é interessante fazer uma array para cada tipo de coisas, pois nao é legal misturar String, com números, com booleano, pois fica difícil e feio o código
+//OBS: para a saúde do código e de quem esta lendo, é interessante fazer uma array para cada tipo de coisas, pois nao é legal misturar String, com números, com booleano, pois fica difícil e feio o código
