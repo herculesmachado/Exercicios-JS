f9faf8b7cb9822238b75d008bf45360a8b4c7fe3
Hércules Machado <herculesprogramador911@gmail.com>
2022-01-06 11:08:14 -0300

    Atualizado

---
+++ Fundamentos/destructuring.js
@@ -0,0 +1,25 @@
+// novo recurso do ES2015
+
+const pessoa = {
+    nome: 'Ana',
+    idade: 24,
+    endereco: {
+        logradouro: 'Rua ABC',
+        numero: 200,
+    }
+}
+
+const {nome, idade} = pessoa // to falando, tire de dentro do objeto (const) os atributo (nome, idade) da const 'Pessoa'
+console.log(nome, idade)
+
+const {nome: n, idade: i} = pessoa
+console.log(n, i)
+
+const {sobrenome, bemHunorada = true} = pessoa
+console.log(sobrenome, bemHunorada)
+
+const { endereco: {logradouro, numero, cep = '(O cep nao possui)'}} = pessoa
+console.log(logradouro, numero, cep)
+
+const {conta: {ag, num}} = pessoa
+console.log(ag, num) // Neste caso aqui, ele da erro pois nao existe o atributo 'conta'
---
+++ Fundamentos/operadorAtribuicao.js
@@ -0,0 +1,17 @@
+const a = 7 // atribuição 'a' ao valor 7
+let b = 3
+
+b += a // b = b + a >> Adição
+console.log(b)
+
+b -= 4 // b = b - 4  >> Subtração
+console.log(b)
+
+b *= 2 // b = b * 2 >> Multiplicação
+console.log(b)
+
+b /= 2 // b = b / 2 >> Divisão
+console.log(b)
+
+b %= 2 // b = b % 2 >> resto de divisão
+console.log(b )
