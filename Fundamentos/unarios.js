3636e9de1fd8d857c88f01cae4b6227ff907160d
Hércules Machado <herculesprogramador911@gmail.com>
2022-01-27 14:18:58 -0300

    Atualizado

---
+++ Fundamentos/unarios.js
@@ -0,0 +1,11 @@
+let num1 = 1
+let num2 = 2
+
+num1++ //Pós fixada (adiciona mais 1 ao numero)
+console.log(num1)
+
+--num1 //Pré fixada (retira menos 1 ao numero)
+console.log(num1)                           
+
+console.log(++num1 === num2--)
+console.log(num1 === num2)
