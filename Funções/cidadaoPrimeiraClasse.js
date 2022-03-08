2c1bcbd47e7ae11a34e3600f405114c24a27cff4
Hércules Machado <herculesprogramador911@gmail.com>
2022-03-08 10:47:31 -0300

    Inclusão de novos arquivos

---
+++ Funções/cidadaoPrimeiraClasse.js
@@ -0,0 +1,41 @@
+//  Função em JS é First-Class Object (Citizens) é quando a linguagem trata a função como um dado
+// Higher-order function (Função de alta ordem) JS funcional
+
+// Criar uma forma literal
+function fun1() { }
+
+// Armazenar em uma variavel
+const fun2 = function() { }
+
+// Armazenar em uma array
+const array = [function (a, b) {return a + b}, fun1, fun2]
+console.log(array[0] (2, 3))
+
+// Armazenar em um atributo de objeto
+const obj = {}
+obj.falar = function() {return 'opa'}
+console.log(obj.falar())
+
+// Passar função como parâmetro
+function run(fun) {
+    fun()
+}
+run(function() {console.log('Executando...')})
+
+// Um função pode retornar / conter uma função
+function soma(a, b) {
+    console.log(a - b)
+    return function(c) {
+        console.log(a + b + c)
+    }
+}
+
+//Formas de como chamar esta função acima
+soma(2, 1)
+soma(4, 2, 5) 
+
+const cincoMais = soma(7, 4)
+cincoMais(3)
+
+
+
