d8b1a997b6e906d6b6fa93bc8edc0905933a822a
Hércules Machado <herculesprogramador911@gmail.com>
2022-05-19 08:40:34 -0300

    atualização

--- Funções/classeVsFuncao.js
+++ Funções/classeVsFuncao.js
@@ -1,3 +1,4 @@
+// Está forma de uso quando utilizado no browser, ao fazer o onclick para o retorno, não retorna o name, pois não reconhece e retorna undefined, sem contar que tem que por (this) para deixar ele publica
 class Pessoa {
     constructor(name) {
         this.name = name
@@ -11,6 +12,8 @@ class Pessoa {
 const p1 = new Pessoa ('Adalberto')
 p1.falar()
 
+
+//Fazendo assim ao ser usado no browser, o name retorna ao ser clicado, e não precisa de (this) na chamado do console.log
 const criarPessoa = name => {
     return{
         falar: () => console.log(`Meu nome é ${name}`)
