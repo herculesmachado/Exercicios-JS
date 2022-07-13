fb3c93bf05ae889f17b988fad4395e8d6adbc5b3
Hércules Machado <herculesprogramador911@gmail.com>
2022-07-13 09:15:18 -0300

    Update notacaoPonto.js

--- Fundamentos/notacaoPonto.js
+++ Fundamentos/notacaoPonto.js
@@ -5,7 +5,7 @@ obj1.nome = 'bola' //Nesse exemplo posso criar uma constante e incrementar, ou s
 //obj1['nome'] = 'bola2'
  console.log(obj1.nome)
 
- function Obj(nome) {
+ function Obj(nome) { //Função
      this.nome = nome // significa; o nome associado ao objeto que foi criado a partir dessa função
      this.exec = function() {
          console.log('Exec...')
@@ -13,7 +13,9 @@ obj1.nome = 'bola' //Nesse exemplo posso criar uma constante e incrementar, ou s
  }
 
 const obj2 = new Obj('cadeira') //depois do new coloca-se o ''Obj'' com a primeira letra maiúscula
-const obj3 = new Obj('Mesa')
+const obj3 = new Obj('Mesa') // quando se coloca new em uma função, e se tornar uma object
 console.log(obj2.nome)
 console.log(obj3.nome)
 obj3.exec()
+
+//Caso eu queira transformar alguma função para object, basta na chamada (EX: const obj3 = new obj('Mesa')) colocar o new antes da função
