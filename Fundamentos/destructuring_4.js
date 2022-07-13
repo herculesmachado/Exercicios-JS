cc976dfb63fd4923f3e4f2acbcd7f6c360bea721
Hércules Machado <herculesprogramador911@gmail.com>
2022-07-13 10:09:29 -0300

    Update destructuring_4.js

--- Fundamentos/destructuring_4.js
+++ Fundamentos/destructuring_4.js
@@ -1,6 +1,6 @@
 function rand([min = 0, max = 100]) {
     if (min > max) [min, max] = [max, min] //O que é min passa a ser max e o que é max passa a ser min
-    const valor = Math.random() * (max - min) + min
+    const valor = Math.random() * (max - min) + min //Em uma função rand eu tenho que sempre utilizar uma const para passar o Math.random 
     return Math.floor(valor)
 }
 console.log(rand([50, 40]))
